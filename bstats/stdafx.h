578b2fc2758a4b0a7c95dbbff968c423ad7f1a44
MaHuJa <github@mahuja.net>
2014-04-08 09:03:36 +0200

    Fixed compile dependencies

--- bstats/stdafx.h
+++ bstats/stdafx.h
@@ -4,9 +4,20 @@
 //
 
 #pragma once
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
 #include <fstream>
 #include <cassert>
-#include <string>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <vector>
+#include <deque>
+#include <map>
+#include <stdexcept>
+#include <algorithm>
 
 #define _CRT_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
